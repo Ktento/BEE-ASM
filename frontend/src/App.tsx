43d1df00c5ac0643b0aecf9e4f03ab8eda60132b
kametsun <kametsun7@gmail.com>
2024-11-28 19:51:55 +0900

    add: form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,31 @@
+import { Box, Button, FormControl, Heading, Input } from "@yamada-ui/react";
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [domain, setDomain] = useState("");
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(domain);
+  };
+
+  return (
+    <Box>
+      <Heading>ASM Tool</Heading>
+      実行ボタン
+      <form onSubmit={handleSubmit}>
+        <FormControl label="対象ドメイン">
+          <Input
+            placeholder="domain"
+            value={domain}
+            onChange={(e) => setDomain(e.target.value)}
+          />
+        </FormControl>
+        <Button type="submit" colorScheme="purple" variant="outline">
+          実行
+        </Button>
+      </form>
+    </Box>
+  );
 }
 
 export default App;
