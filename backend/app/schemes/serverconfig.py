30b98041b91fc8777a8a6a109e178e4c9149a955
Ktento <kento07210525mo@outlook.jp>
2024-12-07 23:13:52 +0900

    DBの設定を追加

--- backend/app/schemes/serverconfig.py
+++ backend/app/schemes/serverconfig.py
@@ -6,3 +6,10 @@ class ServerConfigModel(BaseModel):
 
 	cveapi_base: str = Field("https://cvepremium.circl.lu/api", description="CIRCL CVE Search APIのプロトコルとホスト名を含むベースURL")
 	version: str = Field("0.1.0", description="このASMツールサーバーのバージョン")
+
+	# DB接続情報を設定
+	host:str = Field("localhost",description="EC2のパブリックIPまたはDNS名")
+	port:int = Field(5432,description="PostgreSQLのデフォルトポート")
+	database:str = Field("mbsd",description="データベース名")
+	user:str = Field("mbsd",description="PostgreSQLのユーザー名")
+	password:str = Field("Sangi!Bonvoyage",description="ユーザーパスワード") 
