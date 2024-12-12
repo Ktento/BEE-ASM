8b2a455a32aefac1c17efb8daff4de5393e40678
FukuSuzuki <san-j23023@sangi.jp>
2024-12-12 09:29:13 +0900

    Make some db server configs private

--- backend/app/schemes/serverconfig.py
+++ backend/app/schemes/serverconfig.py
@@ -8,14 +8,25 @@ class ServerConfigModel(BaseModel):
 	cveapi_base: str = Field(default="https://cvepremium.circl.lu/api", description="CIRCL CVE Search APIのプロトコルとホスト名を含むベースURL")
 	version: str = Field(default="0.1.0", description="このASMツールサーバーのバージョン")
 
-	# DB接続情報を設定
-	enable_db:bool=Field(default=True,description="DB接続を行うか")
-	host: str = Field(default="mbsd-db.cpmyoiaqiinr.us-east-1.rds.amazonaws.com", description="EC2のパブリックIPまたはDNS名")
-	port: int = Field(default=5432, description="PostgreSQLのデフォルトポート")
-	database: str = Field(default="mbsd", description="データベース名")
-	user: str = Field(default="mbsd", description="PostgreSQLのユーザー名")
-	password: str = Field(default="Sangi!Bonvoyage", description="ユーザーパスワード")
+	enable_db: bool = Field(default=True, description="DB接続を行うか")
 
 	##### プライベート。隠蔽したいもの #####
 	_gemini_api_key: str = PrivateAttr("AIzaSyABkHvu23Sig59gKjRgd_t8PeJmt30uuQ4")
 	"""Gemini ProのAPIキー"""
+
+	# DB接続情報を設定
+
+	host: str = PrivateAttr("mbsd-db.cpmyoiaqiinr.us-east-1.rds.amazonaws.com")
+	"""EC2のパブリックIPまたはDNS名"""
+
+	port: int = PrivateAttr(5432)
+	"""PostgreSQLのデフォルトポート"""
+
+	database: str = PrivateAttr("mbsd")
+	"""データベース名"""
+
+	user: str = PrivateAttr("mbsd")
+	"""PostgreSQLのユーザー名"""
+
+	password: str = PrivateAttr("Sangi!Bonvoyage")
+	"""ユーザーパスワード"""
